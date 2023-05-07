d1431624c1e35979e7b7f52055f307387bf901ae
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-07 18:12:57 +0530

    feat: create a script file containing all the selectors
    - 'selectors.js' imports required selectors as javascript object

---
+++ src/js/selectors.js
@@ -0,0 +1,6 @@
+const selectors = {
+  hamburgerBtn: document.querySelector(".hamburger-btn"),
+  navigationContainer: document.querySelector(".navigation-container")
+};
+
+export default selectors;
