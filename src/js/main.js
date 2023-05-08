3d1fc5c678b8d179c03b50890714ba99675bb1bb
Rahulkumar <rahulkumar109422@gmail.com>
2023-05-08 19:44:53 +0530

    feat: add functionality to show shadow in header when window is scrolled

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,13 @@ selectors.hamburgerBtn.addEventListener("click", function () {
   this.classList.toggle("animated-hamburger-btn");
   selectors.navigationContainer.classList.toggle("navigation-visible");
 });
+
+// Functionality to add shadow to navigation header
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 50) {
+    selectors.navigationHeader.style.boxShadow =
+      "0 2px 5px hwb(0 0% 100% / .15)";
+  } else {
+    selectors.navigationHeader.style.boxShadow = "none";
+  }
+});
